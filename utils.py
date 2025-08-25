dc89988593f2d51edfa9cd47dea6f21b8907034b
Varun8978 <varunmudhiraj353@gmail.com>
2025-08-25 10:14:20 +0530

    Initial commit - CircleCI practice project

---
+++ utils.py
@@ -0,0 +1,7 @@
+def get_circle_api_key():
+    # Dummy CircleCI API key for practice
+    return "dummy_circleci_api_key"
+
+def get_gh_api_key():
+    # Dummy GitHub API key for practice
+    return "dummy_github_api_key"
